1f47726feff552510ce1a36eed8e11279b81480a
jesse <jessepoleij@hotmail.com>
2025-10-26 17:22:02 +0100

    feat: Add userRole to LoginResponse.

--- src/main/java/com/example/backendmathematicsinc/dto/LoginResponseDTO.java
+++ src/main/java/com/example/backendmathematicsinc/dto/LoginResponseDTO.java
@@ -2,17 +2,20 @@ package com.example.backendmathematicsinc.dto;
 
 
 import com.example.backendmathematicsinc.model.User;
+import com.example.backendmathematicsinc.model.UserRole;
 
 public record LoginResponseDTO(
         String token,
-        String username
+        String username,
+        UserRole userRole
 ) {
 
     public static LoginResponseDTO fromEntityAndToken(User user, String token) {
 
         return new LoginResponseDTO(
                 token,
-                user.getUsername()
+                user.getUsername(),
+                user.getRole()
         );
     }
 }
